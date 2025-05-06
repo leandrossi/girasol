7d3f6c2e797bc104b8e89451397755f3670f529a
Leandro Rossi <leandro.alberto.rossi@gmail.com>
2025-05-06 10:29:44 -0300

    Initial commit with working Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/landingirasol/',
+  base: './',
+  build: {
+    rollupOptions: {
+      input: './src/main.tsx',
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  },
   server: {
     host: '0.0.0.0',
   },
